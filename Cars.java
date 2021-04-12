865932948afe1006b0b793c4135b4b196a9e184f
Andrei - Cristian Wojcicki <56607997+Andreiw99@users.noreply.github.com>
2021-04-12 19:57:07 +0300

    Update Cars.java

--- Cars.java
+++ Cars.java
@@ -1,33 +1,32 @@
 
 public class Car {
 	    private String name;
-	    private int speed;
+	    private int currentSpeed;
+	    private int topSpeed;
 
-	    public Car(String name) {
+	    public Car(String name, int topSpeed) {
 	        this.name = name;
+	        this.topSpeed = topSpeed;
+	        
+	    }
+	    public int getCurrentSpeed() {
+	        return currentSpeed;
+	    }
+	    
+	    public int getTopSpeed() {
+	        return topSpeed;
 	    }
 
-	   
-	    public String getName() {
-	        return name;
+	    public void setCurrentSpeed(int speed) {
+	        this.currentSpeed = speed;
 	    }
-	    public void setName(String name)
-	    {
-	    	this.name = name;
+	    
+	    public void setTopSpeed(int speed) {
+	        this.topSpeed = speed;
 	    }
-	    public int getSpeed() {
-	        return speed;
-	    }
-
-	    public void setSpeed(int speed) {
-	        this.speed = speed;
-	    }
-
-	    public void accelerate() {
-	        speed ++;
-	    }
-
-	    public void brake () {
-	        speed --;
+	
+	   public void accelerate() {
+	    	if(this.currentSpeed < this.topSpeed)
+	    		currentSpeed ++;
 	    }
 }
