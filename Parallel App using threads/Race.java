722098b95e690055b3311746c4d914467b4d0c19
Andrei - Cristian Wojcicki <56607997+Andreiw99@users.noreply.github.com>
2021-04-13 20:37:07 +0300

    Update Race.java

--- Parallel App using threads/Race.java
+++ Parallel App using threads/Race.java
@@ -1,10 +1,9 @@
 public class Race extends Thread {
-	static int noCars=100;
-	Car cars[] = new Car[noCars];
+    static int noCars=100;
+    Car cars[] = new Car[noCars];
     static String name = "car";
     static int speed= 0;
-    static int topSpeed = 4;
-    static int circuit = 50000000;
+    static int topSpeed = 100;
     int ok=0;
     static volatile int gameOver = 0;
     
