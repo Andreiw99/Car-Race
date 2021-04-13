93beae00cde74a2523220a16f674c153d540fdba
Andrei - Cristian Wojcicki <56607997+Andreiw99@users.noreply.github.com>
2021-04-13 20:50:03 +0300

    Update Main.java

--- Parallel App using threads/Main.java
+++ Parallel App using threads/Main.java
@@ -1,9 +1,14 @@
-
 public class Main {
 
 	public static void main(String[] args) {
+		long startTime = System.nanoTime();
 		Race race = new Race();
 		race.start();
+		long endTime = System.nanoTime();
+		long timeElapsed = endTime - startTime;
+		 
+        System.out.println("Execution time in nanoseconds: " + timeElapsed);
+        System.out.println("Execution time in milliseconds: " + timeElapsed / 1000000);
 	}
 
 }
